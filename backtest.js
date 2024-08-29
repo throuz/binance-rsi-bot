2e002c8abac6c519c5e05a7d72b8edf4d0b0888a
HSU JUI SHENG <zed2786328@gmail.com>
2024-08-29 23:22:19 +0800

    display no result when no best result

--- backtest.js
+++ backtest.js
@@ -3,37 +3,43 @@ import { getStepSize } from "./src/helpers.js";
 import { getCachedKlineData, getCachedRsiData } from "./src/cached-data.js";
 
 const bestResult = await getBestResult();
-const {
-  currentPositionType,
-  fund,
-  rsiPeriod,
-  rsiLongLevel,
-  rsiShortLevel,
-  leverage
-} = bestResult;
-console.log("================================================================");
+if (bestResult.fund > 0) {
+  console.log("==============================================================");
+  const {
+    currentPositionType,
+    fund,
+    rsiPeriod,
+    rsiLongLevel,
+    rsiShortLevel,
+    leverage
+  } = bestResult;
 
-const [cachedKlineData, cachedRsiData, stepSize] = await Promise.all([
-  getCachedKlineData(),
-  getCachedRsiData(),
-  getStepSize()
-]);
+  const [cachedKlineData, cachedRsiData, stepSize] = await Promise.all([
+    getCachedKlineData(),
+    getCachedRsiData(),
+    getStepSize()
+  ]);
 
-getBacktestResult({
-  shouldLogResults: true,
-  cachedKlineData,
-  cachedRsiData,
-  stepSize,
-  rsiPeriod,
-  rsiLongLevel,
-  rsiShortLevel,
-  leverage
-});
+  getBacktestResult({
+    shouldLogResults: true,
+    cachedKlineData,
+    cachedRsiData,
+    stepSize,
+    rsiPeriod,
+    rsiLongLevel,
+    rsiShortLevel,
+    leverage
+  });
 
-console.log("================================================================");
-console.log("currentPositionType", currentPositionType);
-console.log("fund", fund);
-console.log("rsiPeriod", rsiPeriod);
-console.log("rsiLongLevel", rsiLongLevel);
-console.log("rsiShortLevel", rsiShortLevel);
-console.log("leverage", leverage);
+  console.log("==============================================================");
+  console.log("currentPositionType", currentPositionType);
+  console.log("fund", fund);
+  console.log("rsiPeriod", rsiPeriod);
+  console.log("rsiLongLevel", rsiLongLevel);
+  console.log("rsiShortLevel", rsiShortLevel);
+  console.log("leverage", leverage);
+} else {
+  console.log("==============================================================");
+  console.log("No result");
+  console.log("==============================================================");
+}
