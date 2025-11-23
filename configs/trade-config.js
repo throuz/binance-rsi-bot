0b8ee6af89bf374bf91a2da045a8e2ccce76731e
Throuz <zed2786328@gmail.com>
2025-11-23 08:56:36 +0800

    update trade configuration parameters for RSI and leverage settings

--- configs/trade-config.js
+++ configs/trade-config.js
@@ -3,10 +3,10 @@ export const QUOTE_ASSET = "USDT";
 export const ORDER_AMOUNT_PERCENT = 100; // 100%
 export const KLINE_INTERVAL = "1h";
 export const KLINE_LIMIT = 1500;
-export const RSI_PERIOD = 55;
-export const RSI_LONG_LEVEL = 69;
-export const RSI_SHORT_LEVEL = 46;
-export const LEVERAGE = 11;
+export const RSI_PERIOD = 23;
+export const RSI_LONG_LEVEL = 66;
+export const RSI_SHORT_LEVEL = 35;
+export const LEVERAGE = 1;
 
 // Only for backtest
 export const INITIAL_FUNDING = 100;
@@ -15,7 +15,7 @@ export const FUNDING_RATE = 0.0001; // 0.01%
 export const RSI_PERIOD_SETTING = { min: 1, max: 100, step: 1 };
 export const RSI_LONG_LEVEL_SETTING = { min: 51, max: 100, step: 1 };
 export const RSI_SHORT_LEVEL_SETTING = { min: 1, max: 50, step: 1 };
-export const LEVERAGE_SETTING = { min: 1, max: 15, step: 1 };
+export const LEVERAGE_SETTING = { min: 1, max: 1, step: 1 };
 export const RANDOM_SAMPLE_NUMBER = null; // number or null
 export const KLINE_START_TIME = getTimestampYearsAgo(10); // timestamp or null
 export const IS_KLINE_START_TIME_TO_NOW = true;
